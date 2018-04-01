5f95d9e147b9f02954a097d200bc84b5b33da5e9
vinit-ww <vineet.kothari@wwindia.com>
2018-04-01 14:48:32 +0530

    Update settings.py

--- neosoft_portfolio/neosoft_portfolio/settings.py
+++ neosoft_portfolio/neosoft_portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5(p^u6m#u%x++-%&(jy&8whce5x8vrp7!1=k!hm=)92346-j5+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
